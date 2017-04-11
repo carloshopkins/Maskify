57745cd6044283340341100cc789ece5fe9412e7
Carlos Hopkins <Carlos Hopkins>
2017-04-10 21:15:49 -0400

    Add project files.

---
+++ Maskify/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maskify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a value to maskify: ");
+            var input = Console.ReadLine();
+            Console.WriteLine(Maskify(input));
+        }
+        static string Maskify(string cc)
+        {
+            if (cc.Length <= 4)
+                return cc;
+            var maskedCC = "";
+            var ccArray = cc.ToCharArray();
+            for (var i = 0; i < ccArray.Length - 4; i++)
+                ccArray[i] = '#';
+            for (int i = 0; i < ccArray.Length; i++)
+                maskedCC += ccArray[i];
+
+            return maskedCC;
+        }
+    }
+}
